d24938fdacb5f8df7e3495cd74205e4205debb55
abrulic1 <abrulic1@etf.unsa.ba>
2023-03-26 19:35:05 +0200

    Minor refactoring

--- frontend/src/components/NavLinks/NavLinks.jsx
+++ frontend/src/components/NavLinks/NavLinks.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './NavLinks.css';
 import ProfileImage from './profile.png';
 import Cart from './cart.png';
@@ -6,21 +7,25 @@ import Web3 from 'web3';
 
 const NavLinks = () =>{
     let web3;
-    const connectWalletHandler = () => {
-        if(typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
-            window.ethereum.request({method: "eth_requestAccounts"});
+    const connectWalletHandler = async () => {
+        if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+          try {
+            await window.ethereum.request({ method: "eth_requestAccounts" });
             web3 = new Web3(window.ethereum);
+          } catch (error) {
+            alert(error.message);
+          }
         } else {
-            //metamask not installed
-            alert('Please install MetaMask');
+          //metamask not installed
+          alert("Please install MetaMask");
         }
     }
 
     return (
      <div className='images-actions'>
-      <button><img src={ProfileImage} alt="Profile" onClick={connectWalletHandler}></img></button>
-      <button><img src={Cart} alt="Cart"></img></button>
-      <button><img src={Wallet} alt="Wallet"></img></button>
+        <button><img src={ProfileImage} alt="Profile" onClick={connectWalletHandler}></img></button>
+        <button><img src={Cart} alt="Cart"></img></button>
+        <button><img src={Wallet} alt="Wallet"></img></button>
      </div>
     )
 };
