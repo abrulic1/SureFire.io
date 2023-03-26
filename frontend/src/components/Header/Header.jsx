ba74393430f3e741f3130d89a322d94b9a7b72ec
abrulic1 <abrulic1@etf.unsa.ba>
2023-03-26 18:35:41 +0200

    Minor changes on the frontend

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ import NavLinks from '../NavLinks/NavLinks';
 import Menu from './menu.png';
 import Close from './close.png';
 import SideDrawer from '../SideDrawer/SideDrawer';
+import EthereumIcon from './ethereum.png';
 
 const Header = ()=>{
   const [isShown, setIsShown] = useState(false);
@@ -15,7 +16,10 @@ const Header = ()=>{
 
     return (
       <div className="header">
-        <h1>ETHin</h1>
+        <div className='logo'>
+          <img src={EthereumIcon} alt="Ethereum"></img>
+          <h1>ETHin</h1>
+        </div>
         <Search />
         <NavLinks />
         <button className='menu' onClick={handleClick}>
