3c06ac2fed63dd64ade92fbd28217d988dcfae98
abrulic1 <abrulic1@etf.unsa.ba>
2023-03-28 13:43:24 +0200

    Updated Product.sol contract

--- smart_contracts/contracts/Product.sol
+++ smart_contracts/contracts/Product.sol
@@ -1,35 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-contract Product{
-    //name, description, price, quantity
-    address public owner;    
-    string public name;
-    string public description;
+contract Product {
     
-    mapping (address => uint) public productBalances;           
+    struct ProductInfo {
+        string name;
+        string description;
+        uint price;
+        uint quantity;
+    }
+    
+    mapping (address => ProductInfo) public products;
+    address public owner;
 
     constructor(){
         owner = msg.sender; 
-        productBalances[address(this)] = 100; 
+        products[address(this)] = ProductInfo("Product", "No description", 0, 0); 
+    }
+    
+    function addProduct(string memory name, string memory description, uint price, uint quantity) public {
+        require(msg.sender == owner, "Only the owner can add the product.");
+        require(price > 0, "Price must be greater than zero.");
+        require(quantity > 0, "Quantity must be greater than zero.");
+        products[address(this)] = ProductInfo(name, description, price, quantity);
+    }
+    
+    function getProduct() public view returns (string memory, string memory, uint, uint) {
+        ProductInfo storage product = products[address(this)];
+        return (product.name, product.description, product.price, product.quantity);
+    }
+    
+    function convertToWei(uint amountInEth) public pure returns (uint) {
+        return amountInEth * 10**15;
     }
 
-    function getVendingMachineBalance() public view returns (uint){
-        return productBalances[address(this)];
-    }
-
-    function restock(uint amount) public {
-        require(msg.sender == owner, "Only the owner can restock this machine.");   //You have to be an owner of this contract to increase amount of donuts on the blockchain.
-        productBalances[address(this)] += amount;
+      function restock(uint amount) public {
+        require(msg.sender == owner, "Only the owner can restock this product.");   
+        require(amount > 0, "Quantity must be greater than zero.");
+        products[address(this)].quantity += amount;
     }
 
     function purchase(uint amount) public payable {
-        //payable - function which needs to receive ether
-
-        require(msg.value >= amount * 0.00001 ether, "You must pay at least 0.00001 ether per donut.");    //2 ether is donut's price 
-        require(amount <= productBalances[address(this)], "Not enough donuts in stock.");
-
-        productBalances[address(this)] -= amount;
-        productBalances[msg.sender] += amount; //purchaser account 
+        //payable - function which needs to receive ethers
+        require(msg.value >= amount * products[address(this)].price, string(abi.encodePacked("You must pay at least ", products[address(this)].price, " Wei per ", products[address(this)].name)));
+        require(amount <= products[address(this)].quantity, "Not enough products in stock.");
+        products[address(this)].quantity -= amount;
+        products[msg.sender].quantity += amount; //purchaser account 
     }
+    
 }
